3a1e578af33b7e7a442b9acd7ea1301d403358f9
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-05-22 22:40:44 -0500

    deescalate privileges

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo yum update -y &&
-    sudo yum groupinstall -y 'Development Tools'
-    su vagrant -c 'curl https://sh.rustup.rs -sSf | sh -s -- -y'
+    sudo yum groupinstall -y 'Development Tools' &&
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
